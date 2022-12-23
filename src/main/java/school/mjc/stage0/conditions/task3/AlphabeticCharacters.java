38ce99bf269d2ce2bc072de1538f9609120ab894
Dmitry Golub <dmgolub@mail.ru>
2022-12-23 23:15:48 +0400

    Task completed.

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -1,6 +1,25 @@
 package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
+
     public void vowelDeterminer(char character) {
+        if (character >= 'a' && character <= 'z' || character >= 'A' && character <= 'Z') {
+            if (isVowel(character)) {
+                System.out.println("Vowel");
+            } else {
+                System.out.println("Consonant");
+            }
+        } else {
+            System.out.println("wrong alphabet!");
+        }
+    }
+
+    private boolean isVowel(char character) {
+        return character == 'a' || character == 'A'
+                || character == 'e' || character == 'E'
+                || character == 'i' || character == 'I'
+                || character == 'o' || character == 'O'
+                || character == 'u' || character == 'U'
+                || character == 'y' || character == 'Y';
     }
 }
